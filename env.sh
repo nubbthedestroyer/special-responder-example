b23ae27a71f666d7b9569b1492a694368ee7761e
nubbthedestroyer <michael@desktopninjas.com>
2018-09-19 01:50:28 -0500

    changed the env.sh to a template

--- env.sh
+++ env.sh
@@ -5,4 +5,4 @@ export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 export AWS_SESSION_TOKEN=$(aws configure get aws_session_token)
 
 export TF_VAR_s3_bucket="lambda-deploy-test-20180918"
-export TF_VAR_function_version="v0.0.6"
+export TF_VAR_function_version="v0.0.10"
